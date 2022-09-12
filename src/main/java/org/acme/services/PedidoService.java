effa0b006bb4e55845748a10294f2fa6472ca6b6
etqletscode <108754163+etqletscode@users.noreply.github.com>
2022-09-12 18:01:12 -0300

    abaixar estoque

--- src/main/java/org/acme/services/PedidoService.java
+++ src/main/java/org/acme/services/PedidoService.java
@@ -5,17 +5,16 @@ import java.util.Set;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.resource.spi.InvalidPropertyException;
 import javax.transaction.Transactional;
 
-import org.acme.converter.ClienteConverter;
 import org.acme.converter.PedidoConverter;
-import org.acme.entity.ClienteEntity;
+import org.acme.entity.ItemPedidoEntity;
 import org.acme.entity.PedidoEntity;
-import org.acme.models.Cliente;
 import org.acme.models.ItemPedido;
 import org.acme.models.Pedido;
 import org.acme.models.PedidoResponse;
-import org.acme.repository.ClienteRepository;
+import org.acme.models.Produto;
 import org.acme.repository.PedidoRepository;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
@@ -34,6 +33,9 @@ public class PedidoService {
     @Inject
     ClienteService clienteService;
 
+    @Inject
+    ProdutoService produtoService;
+
     @Transactional
     public PedidoResponse salvar(Set<ItemPedido> listaProdutos) {
         Pedido pedido = new Pedido();
@@ -44,7 +46,19 @@ public class PedidoService {
         repository.persist(pedidoEntity);
         PedidoResponse response = new PedidoResponse();
         response.setPedidoId(pedidoEntity.getId());
+        processarPedido(pedidoEntity);
         return response;
+    }
+
+    private void processarPedido(PedidoEntity pedidoEntity) {
+        for (ItemPedidoEntity item : pedidoEntity.getListaProdutos()) {
+            Produto produto = produtoService.buscaPorId(item.getId().getProdutoSelecionado().getId());
+            if (item.getQuantidade() > produto.getEstoque()) {
+                throw new IllegalArgumentException();
+            }
+            produto.setEstoque(produto.getEstoque() - item.getQuantidade());
+            produtoService.atualizar(produto);
+        }
     }
 
 }
